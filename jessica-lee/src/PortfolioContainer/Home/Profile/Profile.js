2ca1beccbc6ab1e236204359beac0a4893b1d21c
ellijs <ellijs@hotmail.com>
2021-11-26 01:40:24 -0500

    Add Comments

--- jessica-lee/src/PortfolioContainer/Home/Profile/Profile.js
+++ jessica-lee/src/PortfolioContainer/Home/Profile/Profile.js
@@ -3,6 +3,7 @@ import "./Profile.css";
 import Typical from 'react-typical'
 
 export default function Profile() {
+    // Main Page
     return (
         <div className='profile-container'>
             <div className='profile-parent'>
