835abdadd4f30804ed64ade8635d7ab983ceb77b
ellijs <ellijs@hotmail.com>
2021-11-30 15:43:47 -0500

    About Me

--- jessica-lee/src/PortfolioContainer/AboutMe/AboutMe.js
+++ jessica-lee/src/PortfolioContainer/AboutMe/AboutMe.js
@@ -3,10 +3,69 @@ import ScreenHeading from '../../Utilities/ScreenHeading/ScreenHeading'
 import ScrollService from '../../Utilities/ScrollService';
 import Animations from '../../Utilities/Animations'
 
-function AboutMe() {
+function AboutMe(props) {
+    let fadeInScreenHandler = (screen) => {
+        if(screen.fadeScreen !== props.id) return;
+        Animations.animations.fadeInScreen(props.id)
+    }
+    const fadeInSubscription = ScrollService.currentScreenFadeIn.subscribe(fadeInScreenHandler)
+
+        const SCREEN_CONSTANTS = {
+            description: "Full stack developer. Highly organized, fast learner and problem solver, embarking on software engineering career after 10 years in business management.",
+            highlights : {
+                bullets: [
+                    "Full Stack web development",
+                    "Interactive Front End engineer",
+                    "React JS and State Management",
+                    "Building REST API",
+                    "Effective Back End Ruby on Rails",
+                    "Managing database"
+                ],
+                heading: "Here are few Highlights:"                     
+            }
+        }
+
+        const renderHighlight = () => {
+            return (
+                SCREEN_CONSTANTS.highlights.bullets.map((value, i) => {
+                    return(
+                    <div className = 'highlight' key={i}>
+                        <div className = 'hightlight-blob'>
+                        </div>
+                        <span>{value}</span>
+                    </div>
+                )})
+            )
+        }
+
+
     return (
-        <div>
-            
+        <div className='about-me-container screen-container' id={props.id || ""}>
+            <div className='about-me-parent'>
+                <ScreenHeading title={'About Me'} subHeading={'Why Choose Me?'} />
+                <div className='about-me-card'>
+                    <div className='about-me-profile'></div>
+                    <div className='about-me-details'>
+                        <span className='about-me-description'>{SCREEN_CONSTANTS.description}</span>
+                        <div className='about-me-highlights'>
+                            <div className='highlight-heading'>
+                                <span>{SCREEN_CONSTANTS.highlights.heading}</span>
+                            </div>
+                            {renderHighlight()}
+                        </div>
+                        <div className='about-me-options'>
+                            <button className='btn      primary-btn'>
+                                {""}
+                                Hire Me{" "}
+                            </button>
+                            <a href="https://docs.google.com/document/d/     1543caEa2yocJADDpVmT6Gq9bD--ejZ36lHdmLicnbOs/edit?usp=sharing"      download='Jessica Lee.pdf'>
+                                <button className='btn highlighted-btn'>Get Resume</button>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+           
         </div>
     );
 }
