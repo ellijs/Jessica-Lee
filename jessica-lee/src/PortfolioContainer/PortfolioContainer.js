ae9d6dde62372bac8bbc0c230e4d4019f63a6a62
ellijs <ellijs@hotmail.com>
2021-11-29 23:54:52 -0500

    Creating Component Rendering

---
+++ jessica-lee/src/PortfolioContainer/PortfolioContainer.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import {TOTAL_SCREENS} from '../Utilities/commonUtils'
+
+export default function PortfolioContainer() {
+
+    const mapAllScreens = () => {
+        return(
+            TOTAL_SCREENS.map((screen) => (
+                
+        ))
+        )
+    }
+    return (
+        <div>
+
+        </div>
+    )
+}
