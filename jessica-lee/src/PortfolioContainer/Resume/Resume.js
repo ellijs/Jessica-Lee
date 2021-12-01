246b432da34e53de87ac3901f50361e550ac1703
ellijs <ellijs@hotmail.com>
2021-12-01 13:36:12 -0500

    Refactoring Resume

--- jessica-lee/src/PortfolioContainer/Resume/Resume.js
+++ jessica-lee/src/PortfolioContainer/Resume/Resume.js
@@ -2,7 +2,6 @@ import React, { useState } from "react";
 import ScreenHeading from "../../Utilities/ScreenHeading/ScreenHeading";
 import ScrollService from "../../Utilities/ScrollService";
 import Animations from "../../Utilities/Animations";
-import "./Resume.css";
 
 function Resume(props) {
   const [selectedBulletIndex, setSelectedBulletIndex] = useState(0);
