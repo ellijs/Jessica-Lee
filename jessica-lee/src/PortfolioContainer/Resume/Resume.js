7d28bd61e9ab22c8d44786c34a2993a2f5daa1ff
ellijs <ellijs@hotmail.com>
2021-12-01 13:20:34 -0500

    Resume reusable component

--- jessica-lee/src/PortfolioContainer/Resume/Resume.js
+++ jessica-lee/src/PortfolioContainer/Resume/Resume.js
@@ -4,9 +4,23 @@ import ScrollService from '../../Utilities/ScrollService'
 import Animations from '../../Utilities/Animations'
 import './Resume.css'
 
+
 function Resume(props) {
+    const [selectedBulletIndex, setSelectedBulletIndex] = useState(0)
+    // implement fadeEffect and Smooth scroll
+    const [carousalOffSetStyle, setCarousalOffSetStyle] = useState({})
+
+    let fadeInScreenHandler = (screen) => {
+        if(screen.fadeScreen !== props.id) return;
+        Animations.animations.fadeInScreen(props.id)
+    }
+    const fadeInSubscription = ScrollService.currentScreenFadeIn.subscribe(fadeInScreenHandler)
+
     return (
-        <div>
+        <div className='resume-container screen-container' id={props.id || ""}>
+            <div className='resume-content'>
+                <ScreenHeading title={'Resume'} subHeading={'My Formal Bio Details'} />
+            </div>
             
         </div>
     );
