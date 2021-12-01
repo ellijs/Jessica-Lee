814976e5b1be2194d9e29faa67d3aa6b73cda916
ellijs <ellijs@hotmail.com>
2021-12-01 13:36:42 -0500

    Connecting Resume.css

--- jessica-lee/src/PortfolioContainer/Resume/Resume.js
+++ jessica-lee/src/PortfolioContainer/Resume/Resume.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import ScreenHeading from "../../Utilities/ScreenHeading/ScreenHeading";
 import ScrollService from "../../Utilities/ScrollService";
 import Animations from "../../Utilities/Animations";
+import "./Resume.css";
 
 function Resume(props) {
   const [selectedBulletIndex, setSelectedBulletIndex] = useState(0);
