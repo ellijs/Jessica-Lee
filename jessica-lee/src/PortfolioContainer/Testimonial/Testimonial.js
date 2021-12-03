512969c76f96d252e351d5b46308e2eb7734c849
ellijs <ellijs@hotmail.com>
2021-12-03 13:49:02 -0500

    Adding context to Testimonial

--- jessica-lee/src/PortfolioContainer/Testimonial/Testimonial.js
+++ jessica-lee/src/PortfolioContainer/Testimonial/Testimonial.js
@@ -1,27 +1,48 @@
-import React from 'react';
-import OwlCarousel from 'react-owl-carousel'
+import React from "react";
+import OwlCarousel from "react-owl-carousel";
 import "owl.carousel/dist/assets/owl.carousel.css";
 import "owl.carousel/dist/assets/owl.theme.default.css";
 
-import ScreenHeading from '../../Utilities/ScreenHeading/ScreenHeading'
-import ScrollService from '../../Utilities/ScrollService'
-import Animations from '../../Utilities/Animations'
-
+import ScreenHeading from "../../Utilities/ScreenHeading/ScreenHeading";
+import ScrollService from "../../Utilities/ScrollService";
+import Animations from "../../Utilities/Animations";
 
 // npm i react-owl-carousal
 function Testimonial(props) {
+  let fadeInScreenHandler = (screen) => {
+    if (screen.fadeScreen !== props.id) return;
+    Animations.animations.fadeInScreen(props.id);
+  };
+  const fadeInSubscription = () =>
+    ScrollService.currentScreenFadeIn.subscribe(fadeInScreenHandler);
 
-    let fadeInScreenHandler = (screen) => {
-        if(screen.fadeScreen !== props.id) return;
-        Animations.animations.fadeInScreen(props.id)
-    }
-    const fadeInSubscription = () => ScrollService.currentScreenFadeIn.subscribe(fadeInScreenHandler)
-
-    return (
-        <div>
-            
+  return (
+    <div>
+      <ScreenHeading
+        title={"Testimonial"}
+        subHeading={"What My People Say About Me"}
+      />
+      <section className='testimonial-section' id={props.id || ""}>
+        <div className='container'>
+          <div className='row'>
+            <OwlCarousel className='owl-carousel' id='testimonial-carousel'>
+              <div className='col-lg-12'>
+                <div className='testi-item'>
+                  <div className='testi-comment'>
+                    <p>
+                      I patronized Jessica and when He delivered, I honestly
+                      fell in love with the project. She is a very honest guy
+                      and she delivers ontime.
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </OwlCarousel>
+          </div>
         </div>
-    );
+      </section>
+    </div>
+  );
 }
 
 export default Testimonial;
