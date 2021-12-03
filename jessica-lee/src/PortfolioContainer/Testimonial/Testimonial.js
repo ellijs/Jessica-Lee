17daf328150b27a448563a757c18e701e15c0a9e
ellijs <ellijs@hotmail.com>
2021-12-03 13:40:10 -0500

    Adding Testimonial owl-carousel

--- jessica-lee/src/PortfolioContainer/Testimonial/Testimonial.js
+++ jessica-lee/src/PortfolioContainer/Testimonial/Testimonial.js
@@ -1,6 +1,22 @@
 import React from 'react';
+import OwlCarousel from 'react-owl-carousel'
+import "owl.carousel/dist/assets/owl.carousel.css";
+import "owl.carousel/dist/assets/owl.theme.default.css";
 
+import ScreenHeading from '../../Utilities/ScreenHeading/ScreenHeading'
+import ScrollService from '../../Utilities/ScrollService'
+import Animations from '../../Utilities/Animations'
+
+
+// npm i react-owl-carousal
 function Testimonial(props) {
+
+    let fadeInScreenHandler = (screen) => {
+        if(screen.fadeScreen !== props.id) return;
+        Animations.animations.fadeInScreen(props.id)
+    }
+    const fadeInSubscription = () => ScrollService.currentScreenFadeIn.subscribe(fadeInScreenHandler)
+
     return (
         <div>
             
