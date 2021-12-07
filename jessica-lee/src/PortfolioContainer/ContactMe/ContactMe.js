31e491c108ed5ae27a93e70ba8f4faab2b9187bd
ellijs <ellijs@hotmail.com>
2021-12-07 17:34:25 -0500

    ContactMe

--- jessica-lee/src/PortfolioContainer/ContactMe/ContactMe.js
+++ jessica-lee/src/PortfolioContainer/ContactMe/ContactMe.js
@@ -1,6 +1,20 @@
 import React from 'react';
+import imgBack from '../../../src/images/mailz.jpeg';
+import load1 from '../../../src/images/load2.gif';
+import ScreenHeading from '../../Utilities/ScreenHeading/ScreenHeading';
+import ScrollService from '../../Utilities/ScrollService';
+import Animations from '../../Utilities/Animations';
+
+
 
 function ContactMe(props) {
+    
+    let fadeInScreenHandler = (screen) => {
+        if(screen.fadeScreen !== props.id) return;
+        Animations.animations.fadeInScreen(props.id)
+    }
+    const fadeInSubscription = () => ScrollService.currentScreenFadeIn.subscribe(fadeInScreenHandler)
+
     return (
         <div>
             
