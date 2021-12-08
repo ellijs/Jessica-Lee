856d9a40f33cf0a98662da1e3dacd105e3690533
ellijs <ellijs@hotmail.com>
2021-12-07 22:55:06 -0500

    Reuse Typical and Links

--- jessica-lee/src/PortfolioContainer/ContactMe/ContactMe.js
+++ jessica-lee/src/PortfolioContainer/ContactMe/ContactMe.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import Typical from 'react-typical';
+
 import imgBack from '../../../src/images/mailz.jpeg';
 import load1 from '../../../src/images/load2.gif';
 import ScreenHeading from '../../Utilities/ScreenHeading/ScreenHeading';
@@ -21,6 +23,35 @@ function ContactMe(props) {
             subHeading={"Let's Keep In Touch"}
             title={'Contact Me'}
             />
+            <div className='central-form'>
+                <div className='col'>
+                    <h2 className='title'>
+                        <Typical loop={Infinity} steps={[
+                            'Get In Touch 📧', 
+                                1500, 
+                            'Email Me... 💌',
+                                1500,
+                        ]}
+                        />
+                    </h2>
+                    <a href="https://www.facebook.com/jessica.lee.77128">
+                                <i className='fa fa-facebook-square'></i>
+                            </a>
+                            <a href="https://www.linkedin.com/in/jessica-jisun-lee/">
+                                <i className='fa fa-linkedin'></i>
+                            </a>
+                            <a href="https://twitter.com/ellijs0623">
+                                <i className='fa fa-twitter'></i>
+                            </a>
+                           <a href='https://github.com/ellijs'>
+                                <i className='fa fa-github'></i>
+                            </a>
+                            <a href="https://ellijs0623.medium.com/">
+                                <i className='fa fa-medium'></i>
+                            </a>
+
+                </div>
+            </div>
             
         </div>
     );
