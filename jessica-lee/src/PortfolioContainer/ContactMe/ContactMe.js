4b877039aa610230f78d5b91fb0d51c7c8919fd8
ellijs <ellijs@hotmail.com>
2021-12-07 23:15:28 -0500

    Startting CSS

--- jessica-lee/src/PortfolioContainer/ContactMe/ContactMe.js
+++ jessica-lee/src/PortfolioContainer/ContactMe/ContactMe.js
@@ -6,6 +6,7 @@ import load1 from '../../../src/images/load2.gif';
 import ScreenHeading from '../../Utilities/ScreenHeading/ScreenHeading';
 import ScrollService from '../../Utilities/ScrollService';
 import Animations from '../../Utilities/Animations';
+import './ContactMe.css';
 
 
 
