a32dbe1891787dd95bdf99eed96875b4c8e4ece7
ellijs <ellijs@hotmail.com>
2021-12-09 18:59:54 -0500

    Creating mail router

---
+++ route/contactRoute.js
@@ -0,0 +1,46 @@
+const router = require('express').router();
+const nodemailer = require('nodemailer');
+
+router.post('/contact', (req, res) => {
+    let data = req.body;
+    if (data.name.length === 0 || data.email.length === 0 || data.message.length === 0 ) {
+        return res.json({msg: "Please fill all the fields"})
+    }
+        // 1. create an Obj
+        let smtpTransporter = nodemailer.createTransport({
+            server: 'Gmail',
+            port: 465,
+            auth: { 
+                user: 'ellijs82@gmail.com',
+                pass: ''
+            }
+        })
+
+        let mailOptions = {
+            from: data.email,
+            to: 'ellijs82@gmail.com',
+            subject: `message from $(data.name)`,
+            html: `
+            
+            <h3>Informations</h3>
+            <ul>
+            <li> Name: ${data.name}</li>
+            <li> Email: ${data.email}</li>
+            </ul>
+            <h3>Message</h3>
+            <p>
+            ${data.message}
+            </p>
+            `
+        }
+
+        smtlTransporter.sendMail(mailOptions, (error) => {
+            try {
+                if(error) return res.status(400).json({msg: "Please fill out all the fields"})
+                res.status(200).json({msg: "Thank you for contacting Jessica!"})
+            } catch (error) {
+                if(error) return res.status(500).json({msg: "There is server error"})
+            }
+        })
+})
+module.exports = router
--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const express = require("express");
 const cors = require("cors");
 const path = require("path");
 
+const contactRoute = require('./route/contactRoute');
+
 const app = express();
 
 // creating the middleware
