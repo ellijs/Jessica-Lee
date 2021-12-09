994f3059dd0a5de26ff411f19ebd6c2259d08e45
ellijs <ellijs@hotmail.com>
2021-12-09 18:41:32 -0500

    creating server

---
+++ server.js
@@ -0,0 +1,15 @@
+require("dotenv").config
+const express = require("express");
+const cors = require("cors");
+const path = require("path");
+
+const app = express();
+
+// creating the middleware
+app.use(express.json());
+app.use(cors());
+
+// creating port
+
+const port = process.env.PORT || 5000;
+app.listen(port, console.log(`server listing to port 5000`));
